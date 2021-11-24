717d93eddb707eb00768b41cc505343bf35d2eed
HoangPhuc1608 <phucvh2001@gmail.com>
2021-11-24 09:27:03 +0700

    Source code

---
+++ Buoi08_CCPTPM/Buoi8_CCPTPM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Buoi8_CCPTPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
