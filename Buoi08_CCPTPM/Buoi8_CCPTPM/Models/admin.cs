bb147636c884a1b46e1744b86ae4096ed487bb64
PhucSmRePo <0306191060@caothang.edu.vn>
2021-11-24 10:04:01 +0700

    admin

---
+++ Buoi08_CCPTPM/Buoi8_CCPTPM/Models/admin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi8_CCPTPM.Models
+{
+
+    public class admin : member
+    {
+        public static int idAdmin;
+        public static string name;
+    }
+    public static void dangNhap()
+    {
+
+    }
+    public static void timKiemTV()
+    {
+
+    }
+    public static void chinhSuaTV()
+    {
+
+    }
+    public static void xoaTV()
+    {
+
+    }
+    public static void chinhSuaGD()
+    {
+
+    }
+    public static void timBaiViet()
+    {
+
+    }
+    public static void themBaiViet()
+    {
+
+    }
+    public static void chinhSuaBaiViet()
+    {
+
+    }
+    public static void xoaBaiViet()
+    {
+
+    }
+
+}
