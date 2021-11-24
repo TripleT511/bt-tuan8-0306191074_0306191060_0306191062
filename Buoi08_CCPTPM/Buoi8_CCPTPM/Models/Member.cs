cf3af1338ebf8f4d2c7f0090a06ef19a5d397ee3
TripleT511 <0306191074@caothang.edu.vn>
2021-11-24 10:16:44 +0700

    Add member class

---
+++ Buoi08_CCPTPM/Buoi8_CCPTPM/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi8_CCPTPM.Models
+{
+    public class Member : User
+    {
+        public void DangKy() { }
+        public void DangNhap() { }
+        public void BinhLuan() { }
+        public void UploadTaiLieu() { }
+        public void TaoTopic() { }
+        public void ChinhSuaTopic(int id) { }
+        public void XoaTopic(int id) { }
+        public void CapNhatThongTin(int id) { }
+        public void DoiMatKhau() { }
+    }
+}
